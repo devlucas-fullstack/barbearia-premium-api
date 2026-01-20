9cf74a70cb6b41c5edb6ef7cd5e127f00af7ef87
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-20 15:56:55 -0300

    feat: creates authentication token

--- src/controllers/session-controller.ts
+++ src/controllers/session-controller.ts
@@ -1,8 +1,39 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { compare } from "bcrypt";
+import { AppError } from "@/utils/AppError";
+import { jwtConfig } from "@/configs/jwt";
+import { sign } from "jsonwebtoken";
 
 class SessionController {
   async create(req: Request, res: Response) {
-    res.json("teste");
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string(),
+    });
+
+    const { email, password } = bodySchema.parse(req.body);
+
+    const user = await prisma.user.findFirst({ where: { email } });
+
+    if (!user) {
+      throw new AppError("Email e/ou senha inválidos!", 401);
+    }
+
+    const matchedPassword = await compare(password, user.password);
+
+    if (!matchedPassword) {
+      throw new AppError("Email e/ou senha inválidos!", 401);
+    }
+
+    const { secret, expiresIn } = jwtConfig;
+    const token = sign({ role: user.role }, secret, {
+      expiresIn,
+      subject: String(user.id),
+    });
+
+    res.status(201).json({ user, token });
   }
 }
 
