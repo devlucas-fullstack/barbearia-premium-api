739854f11c802b26af35f107ecc1f46b82b20fe2
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-20 12:27:40 -0300

    feat: create server

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+import "express-async-errors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3333, () => console.log("Servidor rodando na porta 3333"));
